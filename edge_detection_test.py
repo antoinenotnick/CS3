84d3c2bd32f5586b1da82d39f28e804872a2c9c0
antoinenotnick <anthonytstan01@gmail.com>
2025-07-15 23:36:13 -0400

    Added gitignore and test .py files

--- edge_detection_test.py
+++ edge_detection_test.py
@@ -1,36 +1,64 @@
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
 
-img = cv2.imread("CS3_Project\testimg.jpg", cv2.IMREAD_GRAYSCALE)
-print(img)
-
-# Apply Sobel operator
-sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)  # Horizontal edges
-sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)  # Vertical edges
-
-# Compute gradient magnitude
-gradient_magnitude = cv2.magnitude(sobelx, sobely)
- 
-# Convert to uint8
-gradient_magnitude = cv2.convertScaleAbs(gradient_magnitude)
- 
-# Display result
-cv2.imshow("Sobel Edge Detection", gradient_magnitude)
- 
+# Read the original image
+img = cv2.imread('ex.jpg')
+# Display original image
+cv2.imshow('Original', img)
 cv2.waitKey(0)
+
+# Convert to graycsale
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Blur the image for better edge detection
+img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0)
+
+# Canny Edge Detection
+edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)  # Canny Edge Detection
+# Display Canny Edge Detection Image
+cv2.imshow('Canny Edge Detection', edges)
+cv2.waitKey(0)
+
+# Apply Laplacian operator
+laplacian = cv2.Laplacian(img, cv2.CV_64F)
+
+# Convert to uint8
+laplacian_abs = cv2.convertScaleAbs(laplacian)
+
+# Display result
+cv2.imshow("Laplacian Edge Detection", laplacian_abs)
+
+cv2.waitKey(0)
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    gray_vid = cv2.cvtColor(frame, cv2.IMREAD_GRAYSCALE)
+    cv2.imshow('Original', frame)
+    edged_frame = cv2.Canny(frame, 100, 200)
+    cv2.imshow('Edges', edged_frame)
+    cv2.waitKey(0)
+
+    # Apply Laplacian operator
+    laplacian = cv2.Laplacian(frame, cv2.CV_64F)
+
+    # Convert to uint8
+    laplacian_abs = cv2.convertScaleAbs(laplacian)
+
+    # Display result
+    cv2.imshow("Laplacian Edge Detection", laplacian_abs)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
 cv2.destroyAllWindows()
-
-# threshold = 6.
-
-# img = cv2.imread('testimg.JPG')
-
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) / 255
-
-# sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=5)
-# sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=5)
-# sobel_xy = np.sqrt(sobelx ** 2 + sobely ** 2)
-# edge_image = np.float32(sobel_xy > threshold)
-
-# plt.imshow(edge_image, 'gray')
-# plt.show()
